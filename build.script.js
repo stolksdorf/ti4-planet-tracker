a23d0701142a4f116522fd006fc97e5bba47c528
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-09-04 17:47:28 -0400

    cooking some routes

--- build.script.js
+++ build.script.js
@@ -1,13 +1,18 @@
 const build = require('vitreum/build');
 const fs = require('fs');
 
+String.prototype.replaceAll = function (find, replace) {
+	var str = this;
+	return str.replace(new RegExp(find, 'g'), replace);
+};
 
 build().then(()=>{
-
-	console.log('done!');
-
 	const render = require('./docs/main/render.js');
 
-	fs.writeFileSync('./docs/index.html', render())
-
+	fs.writeFileSync('./docs/index.html', render()
+		.replace('/libs.js', 'docs/libs.js')
+		.replaceAll('/main/bundle', 'docs/main/bundle')
+		.replaceAll('/assets', 'docs/assets')
+	)
+	console.log('Built index.html');
 })
